2f9fe3631ae07a412302d8e107584e4061439186
gianlucafalaschi <gian100gian@outlook.it>
2024-05-21 17:29:46 +0200

    controller and model created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+
+Route::get('/', [PageController::class, 'index']);
+
+
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
