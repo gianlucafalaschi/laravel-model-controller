046a5812261274b1170376dce6ae0afcf94dc6d2
gianlucafalaschi <gian100gian@outlook.it>
2024-05-21 17:42:26 +0200

    home created, welcome deleted

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">Sono il main di home</div>
+        </div>
+    </div>
+    
+@endsection
