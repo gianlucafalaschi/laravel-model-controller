01b7b9e6bc98f90b19825a506aa09821aabaa90d
gianlucafalaschi <gian100gian@outlook.it>
2024-05-21 18:45:16 +0200

    cards printed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,22 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col">Sono il main di home</div>
+            @foreach ($movies as $movie)
+            <div class="col">
+                <div class="card my-3" style="width: 18rem;">
+                    <div class="card-body bg-primary">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">id: {{$movie->id}}</li>
+                            <li class="list-group-item">Titolo: {{$movie->title}}</li>
+                            <li class="list-group-item">Titolo Originale: {{$movie->original_title}} </li>
+                            <li class="list-group-item">Nazionalità: {{$movie->nationality}} </li>
+                            <li class="list-group-item">Data: {{$movie->date}} </li>
+                            <li class="list-group-item">Voto: {{$movie->vote}}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
     
