e05dfa81ef73f6d1ad25df38643fc21ec7c9d435
gianlucafalaschi <gian100gian@outlook.it>
2024-05-21 18:13:34 +0200

    function to get the movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index() {
+
+        $movies = Movie::all();
         
+        // dd($movies);
+        
+
+        $data = [
+            'movies' => $movies
+        ];
+
+
+        return view('home',$data);
     }
 }
